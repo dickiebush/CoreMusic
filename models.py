17c64c57b8a486273c1aff2776ccb9f2d6bfd4fd
Richard Bush <rjb2@princeton.edu>
2017-01-02 01:07:39 -0500

    running on its own sending to me

---
+++ models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class song(db.Model):
+    __tablename__ = 'songs'
+    url       = db.Column(db.String(150), primary_key = True)
+    song_name = db.Column(db.String(150))
+    artist    = db.Column(db.String(150))
+
+    def __init__(self, url, song_name, artist):
+        self.url = url.lower()
+        self.song_name = song_name.lower()
+        self.artist = artist.lower()
+
+    def __str__(self):
+        return self.url
--- routes.py
+++ routes.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template
+from models import db, song
+import core
 
+# magical incantations 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/songs'
+    # Extensions like Flask-SQLAlchemy now know what the "current" app
+    # is while within this block. Therefore, you can now run........
+db.init_app(app)
 
+#core.run_script(conn,db)
 @app.route("/")
 def index():
+    core.run_script(db)
     return ("Bout to run some shit nigga")
+
+if __name__ == "__main__":
+    app.run(debug=True)
