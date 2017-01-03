0a68bf34aa0997e9dd20d9b2a896959d5a0477d6
Richard Bush <rjb2@princeton.edu>
2017-01-03 17:34:22 -0500

    launch

--- forms.py
+++ forms.py
@@ -3,9 +3,9 @@ from wtforms import StringField, TextAreaField, SubmitField, BooleanField, Passw
 from wtforms import validators 
 
 class SignupForm(FlaskForm):
-    email  = StringField('Email:', validators=[validators.DataRequired(), validators.email()])
-    name   = StringField('Full Name:', validators=[validators.DataRequired("Please enter your name")])
-    number = StringField('Phone Number:', validators=[validators.DataRequired("Please enter your phone number"), \
+    email  = StringField('Email:', validators=[validators.DataRequired("A valid email is required"), validators.email("A valid email is required")])
+    name   = StringField('Full Name:', validators=[validators.DataRequired("Your full name is required")])
+    number = StringField('Phone Number:', validators=[validators.DataRequired("A valid 10-digit phone number is required"), \
                          validators.Length(10,10,"Please enter a 10 digit phone number"), \
                          validators.Regexp('[0-9]{10}')])
     artists = TextAreaField('Enter your favorite music artists, separated by commas', validators=[validators.DataRequired("Please enter your favorite artists")])
